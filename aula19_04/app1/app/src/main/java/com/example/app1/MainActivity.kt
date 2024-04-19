bd39ee1b780ccad8090ab7942aba4761aa6108eb
P4BLOll <pablosousa386@gmail.com>
2024-04-19 17:59:59 -0300

    Exemplos da aula de PAM-ll

---
+++ aula19_04/app1/app/src/main/java/com/example/app1/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.app1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.app1.componentes.simpleTextField
+import com.example.app1.ui.theme.App1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            simpleTextField()
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    App1Theme {
+        Greeting("Android")
+    }
+}
